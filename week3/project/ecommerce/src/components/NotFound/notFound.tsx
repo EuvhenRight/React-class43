fb9f59f72000c5930314156c26e0d600943cd147
EuvhenRight <ugnivenko.ea@gmail.com>
2023-08-03 21:17:14 +0200

    final

--- week3/project/ecommerce/src/components/NotFound/notFound.tsx
+++ week3/project/ecommerce/src/components/NotFound/notFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './NotFound.module.css';
 
-const NotFound = () => {
+const NotFound: React.FC = () => {
   return <div className={style.main}>Page is not found</div>;
 };
 
